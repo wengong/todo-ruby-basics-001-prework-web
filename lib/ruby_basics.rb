880ec64adefe0ed87e17158e8f2c19b2a6f2d390
wengong <wentoshare@gmail.com>
2016-03-05 04:42:57 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  ruturn num1/num2
 end
 
 def assign_variable(value)
-  
+  return a=value
 end
 
-def argue
-  
+def argue(arg)
+  return arg
 end
 
-def greeting
-
+def greeting(arg="hello")
+  return arg
 end
 
-def return_a_value
-
+def return_a_value(value)
+  return value
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(value="0")
+  return value
 end
 
-def pizza_party
-
+def pizza_party(pp)
+  return pp
 end
